6c99b26b27b99149d5a7caa3317e8050dc41575e
Jakub Kozłowski <kubukoz@gmail.com>
2019-02-16 02:55:49 +0100

    Improvements

--- app/Boards.hs
+++ app/Boards.hs
@@ -19,12 +19,13 @@ newtype Coords =
   Coords (Sum Int, Sum Int)
   deriving (Semigroup)
 
-newtype Rows =
-  Rows [[Maybe Int]]
+newtype Rows = Rows
+  { extractRows :: [[Maybe Int]]
+  }
 
-newtype Current =
-  Current Int
-  deriving (Show)
+newtype Current = Current
+  { extractCurrent :: Int
+  } deriving (Show)
 
 data Board = Board
   { _rows    :: Rows
@@ -38,7 +39,7 @@ makeBoard = Board
 makeLenses ''Board
 
 instance Show Rows where
-  show (Rows (rows)) = unlines . fmap (unwords . fmap (maybe " " show)) $ rows
+  show (Rows rows) = unlines . fmap (unwords . fmap (maybe " " show)) $ rows
 
 instance Show Coords where
   show (Coords (Sum x, Sum y)) = "(" ++ show x ++ ", " ++ show y ++ ")"
@@ -46,73 +47,68 @@ instance Show Coords where
 instance Show Board where
   show b = show (view rows b) ++ "\n" ++ show (view coords b) ++ "\n" ++ show (view current b) ++ "\n"
 
-newtype Move =
-  Move Coords
+newtype Move = Move
+  { toCoords :: Coords
+  }
 
 allMoves :: [Move]
 allMoves = fmap (Move . uncurry newCoords) [(-3, 0), (0, 3), (3, 0), (0, -3), (-2, 2), (2, 2), (2, -2), (-2, -2)]
 
-toCoords :: Move -> Coords
-toCoords (Move coords) = coords
-
 newCoords :: Int -> Int -> Coords
 newCoords x y = Coords (Sum x, Sum y)
-
-extractRows :: Rows -> [[Maybe Int]]
-extractRows (Rows a) = a
 
 extractCoords :: Coords -> (Int, Int)
 extractCoords (Coords (Sum x, Sum y)) = (x, y)
 
-extractCurrent :: Current -> Int
-extractCurrent (Current c) = c
-
 modifyAt :: Int -> (a -> a) -> [a] -> [a]
 modifyAt _ _ []     = []
-modifyAt 0 f (x:xs) = (f x) : xs
+modifyAt 0 f (x:xs) = f x : xs
 modifyAt n f (x:xs) = x : modifyAt (n - 1) f xs
 
-fits :: Int -> Int -> Int -> Bool
+fits :: Ord a => a -> a -> a -> Bool
 fits what from until = what >= from && what < until
 
 moveCoords :: Move -> Coords -> Coords
 moveCoords = (<>) . toCoords
 
+condMaybe :: (a -> Bool) -> (a -> b) -> a -> Maybe b
+condMaybe pred f a
+  | pred a = Just (f a)
+  | otherwise = Nothing
+
 moveBoard :: Board -> Move -> Maybe Board
-moveBoard b m =
-  case over coords (moveCoords m) b of
-    moved
-      | inRange moved -> Just $ markNumber moved
-    _ -> Nothing
+moveBoard b m = condMaybe inRange markNumber (over coords (moveCoords m) b)
 
 emptyBoard :: Int -> Board
 emptyBoard n = markNumber $ makeBoard table (newCoords 0 0) (Current 0)
   where
-    table = Rows $ tabs [1 .. n]
-    tabs arr = Nothing <$ arr <$ arr
+    table = Rows tabs
+    tabs = replicate n . replicate n $ Nothing
 
 markNumberAt :: Coords -> Current -> Rows -> Rows
 markNumberAt coordies curr = Rows . marked . extractRows
   where
     (x, y) = extractCoords coordies
-    marked = modifyAt y (modifyAt x (const . Just . extractCurrent $ curr))
+    marked = modifyAt y . modifyAt x . (const . Just . extractCurrent) $ curr
 
 modifyBoardRows :: (Rows -> Rows) -> Board -> Board
 modifyBoardRows = over rows
 
 markNumber :: Board -> Board
-markNumber board = over rows (markNumberAt currentCoords newCurrent) . set current newCurrent $ board
+markNumber board = putNumber . setCurrent $ board
   where
     currentCoords = view coords board
     newCurrent = Current . (1 +) . extractCurrent . view current $ board
+    putNumber = over rows (markNumberAt currentCoords newCurrent)
+    setCurrent = set current newCurrent
 
 inRange :: Board -> Bool
-inRange b = fitsX && fitsY && notTaken
+inRange b = fitsX && fitsY && not taken
   where
     (x, y) = extractCoords . view coords $ b
     fitsX = fits x 0 (boardWidth b)
     fitsY = fits y 0 (boardHeight b)
-    notTaken = isNothing $ (extractRows . view rows $ b) !! y !! x
+    taken = isJust $ (extractRows . view rows $ b) !! y !! x
 
 boardHeight :: Board -> Int
 boardHeight = length . extractRows . view rows
@@ -123,22 +119,25 @@ boardWidth = sum . fmap length . listToMaybe . extractRows . view rows
 isComplete :: Board -> Bool
 isComplete = all (all isJust) . extractRows . view rows
 
+orElse :: Maybe a -> Maybe a -> Maybe a
+orElse a@(Just _) _ = a
+orElse _ b          = b
+
 explore :: Board -> Maybe Board
 explore board =
   case moved of
-    [] -> Nothing
-    remaining ->
-      case find isComplete remaining of
-        Nothing   -> listToMaybe $ mapMaybe explore remaining
-        completed -> completed
+    []        -> Nothing
+    remaining -> orElse (findCompleted remaining) (exploreDeeper remaining)
   where
-    moved = mapMaybe (moveBoard board) $ allMoves
+    moved = mapMaybe (moveBoard board) allMoves
+    findCompleted = find isComplete
+    exploreDeeper = listToMaybe . mapMaybe explore
 
 showSolution :: Int -> IO ()
 showSolution = maybe (pure ()) print . explore . emptyBoard
 
 parseArg :: IO (Maybe Int)
-parseArg = fmap listToMaybe getArgs >>= (traverse readIO)
+parseArg = fmap listToMaybe getArgs >>= traverse readIO
 
 executeBoardProgram :: IO ()
-executeBoardProgram = fmap (fromMaybe 5) parseArg <* (putStrLn "Started work") >>= (liftIO . showSolution)
+executeBoardProgram = fromMaybe 5 <$> parseArg <* putStrLn "Started work" >>= showSolution
--- app/Main.hs
+++ app/Main.hs
@@ -11,40 +11,6 @@ import           Data.Foldable
 import           Data.Monoid
 import           Lib
 
-newtype Username = Username
-  { extractU :: String
-  } deriving (Show)
-
-data User = User
-  { username :: Username
-  , age      :: Int
-  }
-
-tupleToUser (name, age) = User (Username name) age
-
-data NonEmptyList a =
-  NonEmptyList a
-               [a]
-  deriving (Show, Functor, Foldable)
-
-tuples = NonEmptyList ("kubukoz", 21) [("kumalg", 22), ("starbuxman", 34), ("g00bm4n", 22)]
-
-friends = fmap tupleToUser tuples
-
-prog1 = print . fmap extractU . fmap username $ friends
-
-prog2 = print . getSum . foldMap (Sum . age) $ friends
-
-div3 a = rem a 3 == 0
-
-prog3 = do
-  count <- getLine >>= readIO
-  let numbers = filter div3 . take count $ [1 ..]
-  traverse_ print numbers
-  when (null numbers) retry
-  where
-    retry = (putStrLn "Didn't find any, try again" *> prog3)
-
 main :: IO ()
 main = executeBoardProgram
 --main = putStrLn "Hello World" *> prog1 *> prog2 *> prog3
